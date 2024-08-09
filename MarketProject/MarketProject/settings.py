fca8f0a50edd1b0b77fc8f3dfba26e2f67112c22
Julian Lozano <julianlozanon48@gmail.com>
2024-08-08 19:01:04 -0500

    Se comenta clave Django para analisis de Sonar

--- MarketProject/MarketProject/settings.py
+++ MarketProject/MarketProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^cw6-fg)04dmg%c%s2w9%kjw@kxrd)omymst=tjqd@x2015y0'
+#SECRET_KEY = 'django-insecure-l^cw6-fg)04dmg%c%s2w9%kjw@kxrd)omymst=tjqd@x2015y0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
